e7b8d0971e2ea7efdb9dcb98567e8b10f70b4a58
Leone Oliveira <falecom@leone.tec.br>
2022-07-04 15:59:16 -0300

    Adiciona oo bot de alerta no portifólio

--- index.php
+++ index.php
@@ -53,9 +53,9 @@
             a minha vida e das pessoas ao meu redor. O tempo passou e só cresceu junto comigo essa vontade de entender a
             tecnologia e usá-la para simplificar a vida das pessoas, o caminho que eu encontrei para cumprir minha
             missão de ajudar as pessoas através da tecnologia foi me tornando desenvolvedor.</p>
-        <div class="text-center mt-5"><a href="#apps">
-                <button class="btn btn-outline-dark btn-lg">Conhecer meus <i>apps</i>/jogos</button>
-            </a></div>
+        <div class="text-center mt-5">
+            <a href="#apps" class="btn btn-outline-dark btn-lg">Conhecer meus <i>apps</i>/jogos</a>
+        </div>
     </div>
     <div class="product-device shadow-sm d-none d-lg-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-lg-block"></div>
@@ -77,14 +77,33 @@
                 categorias e até mesmo rastrear seu pedido se ele tiver sido enviado pelos Correios.</p>
         </div>
         <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://ofertas.leone.tec.br/">
-                    <button class="btn btn-outline-light bg-lg">Acessar agora</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/leone-promos">
-                    <button class="btn btn-outline-light bg-lg">Ver código</button>
-                </a></div>
+            <div>
+                <a href="https://ofertas.leone.tec.br/" class="btn btn-outline-light bg-lg">Acessar agora</a>
+            </div>
+            <div>
+                <a href="https://github.com/leonetecbr/leone-promos" class="btn btn-outline-light bg-lg">Ver código</a>
+            </div>
         </div>
     </div>
+    <div class="bg-light mr-md-3 px-3 px-md-5 text-center overflow-hidden" id="federal">
+        <div class="bg-dark shadow-sm mx-auto p-3 mb-3"
+             style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
+            <img src="/img/bot_alert.png" alt="Bot de alerta">
+        </div>
+        <h2 class="display-5">Bot de Alerta</h2>
+        <div class="my-3 p-3">
+            <p class="lead">Esse bot permite aos usuários gerenciarem quais tópicos querem ser alertados a qualquer
+                momento, permite que qualquer usuário lance um alerta para avisar a todos quando algo importante
+                acontecer no grupo do WhatsApp, além disso, os tópicos são facilmente customizáveis.</p>
+        </div>
+        <div class="d-grid mb-3 p-3">
+            <a href="https://github.com/leonetecbr/bot-alert-group-whatsapp/" class="btn btn-outline-dark bg-lg btn-block">
+                Ver código
+            </a>
+        </div>
+    </div>
+</div>
+<div class="d-md-flex flex-md-equal w-100 pl-md-3">
     <div class="bg-light mr-md-3 px-3 px-md-5 text-center overflow-hidden" id="federal">
         <div class="bg-dark shadow-sm mx-auto p-3 mb-3"
              style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
@@ -98,18 +117,17 @@
                 usuário deseje.</p>
         </div>
         <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://www.amazon.com.br/dp/B099X7D5NC">
-                    <button class="btn btn-outline-dark bg-lg">Ativar skill</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/leone-promos">
-                    <button class="btn btn-outline-dark bg-lg">Ver código</button>
-                </a></div>
+            <div>
+                <a href="https://www.amazon.com.br/dp/B099X7D5NC" class="btn btn-outline-dark bg-lg">Ativar skill</a>
+            </div>
+            <div>
+                <a href="https://github.com/leonetecbr/alexa-resultado-federal/" class="btn btn-outline-dark bg-lg">Ver código</a>
+            </div>
         </div>
     </div>
-</div>
-<div class="d-md-flex flex-md-equal w-100 pl-md-3">
-    <div class="bg-light mr-md-3 px-3 px-md-5 text-center overflow-hidden" id="megasena">
-        <div class="bg-dark shadow-sm mx-auto mb-3"
+
+    <div class="bg-dark text-light mr-md-3 px-3 px-md-5 text-center overflow-hidden" id="megasena">
+        <div class="bg-light shadow-sm mx-auto mb-3"
              style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
             <img src="/apps/megasena/icons/192.png" alt="Mega-Sena">
         </div>
@@ -118,61 +136,40 @@
             <p class="lead">Com grande parte das pessoas querendo apostar na Mega-sena da virada e diante da
                 impossibilidade de geração dos números diretamente pelo sistema da CAIXA, criei esse pequeno sistema que
                 gera os números para quem quiser apostar na Mega-Sena, além disso, há opções de personalização do modo
-                de
-                geração dos números.</p>
+                de geração dos números.</p>
         </div>
         <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://leone.tec.br/apps/megasena/">
-                    <button class="btn btn-outline-dark bg-lg">Acessar agora</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/gera-numeros-mega-sena">
-                    <button class="btn btn-outline-dark bg-lg">Ver código</button>
-                </a></div>
+            <div>
+                <a href="https://leone.tec.br/apps/megasena/" class="btn btn-outline-light bg-lg">Acessar agora</a>
+            </div>
+            <div>
+                <a href="https://github.com/leonetecbr/gera-numeros-mega-sena" class="btn btn-outline-light bg-lg">
+                    Ver código
+                </a>
+            </div>
         </div>
     </div>
+</div>
+
+<div class="d-md-flex flex-md-equal w-100 pl-md-3">
     <div class="bg-dark mr-md-3 px-3 px-md-5 text-center text-light overflow-hidden" id="snake">
         <div class="bg-light shadow-sm mx-auto mb-3"
              style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
             <img src="/games/snake/img/192.png" alt="Jogo da cobrinha">
         </div>
-        <h2 class="display-5">Jogo da cobrinha</h2>
+        <h2 class="display-5">Jogo da Cobrinha</h2>
         <div class="mt-3 py-3">
             <p class="lead">O clássico jogo da cobrinha recriado para o curso de HTML Web Developer que fiz na Digital
                 Innovation One. Fiz algumas adições ao jogo original como adição das bombas que fazem perder pontuação e
                 o teclado virtual para dispositivos mobile.</p>
         </div>
         <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://leone.tec.br/games/snake">
-                    <button class="btn btn-outline-light bg-lg">Jogar agora</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/tic-tac-toe">
-                    <button class="btn btn-outline-light bg-lg">Ver código</button>
-                </a></div>
-        </div>
-    </div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 pl-md-3">
-    <div class="bg-dark mr-md-3 px-3 px-md-5 text-center text-light overflow-hidden" id="resgate">
-        <div class="bg-light shadow-sm mx-auto p-3 mb-3"
-             style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
-            <img src="/games/rescue/imgs/icons/192.png" alt="Jogo de resgate">
-        </div>
-        <h2 class="display-5">Jogo de Resgate</h2>
-        <div class="my-3 p-3">
-            <p class="lead">Seguindo as orientações do professor da Digital Innovation One, construir esse jogo onde o
-                objetivo é regatar o amigo e eliminar a maior quantidade de inimigos possíveis. A dificuldade aumenta
-                sempre que o jogador consegue mais pontos até ele perder todas suas energias, além disso, é possível
-                jogar <i>offline</i>.
-            </p>
-        </div>
-        <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://leone.tec.br/games/rescue">
-                    <button class="btn btn-outline-light bg-lg">Jogar agora</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/rescue-dio">
-                    <button class="btn btn-outline-light bg-lg">Ver código</button>
-                </a></div>
+            <div>
+                <a href="https://leone.tec.br/games/snake" class="btn btn-outline-light bg-lg">Jogar agora</a>
+            </div>
+            <div>
+                <a href="https://github.com/leonetecbr/snake-dio" class="btn btn-outline-light bg-lg">Ver código</a>
+            </div>
         </div>
     </div>
     <div class="bg-light mr-md-3 px-3 px-md-5 text-center overflow-hidden" id="tictactoe">
@@ -180,22 +177,44 @@
              style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
             <img src="/img/tictactoe.png" alt="Jogo da velha">
         </div>
-        <h2 class="display-5">Jogo da velha</h2>
+        <h2 class="display-5">Jogo da Velha</h2>
         <div class="my-3 p-3">
             <p class="lead">O bastante conhecido jogo da velha agora pode ser jogado sem você precisar instalar
                 nenhum aplicativo! Tudo pelo navegador, mas ainda sim, você poderá chamar seu amigo para jogar contra
                 você ou qualquer pessoa do mundo conectada a <i>internet</i>.</p>
         </div>
         <div class="d-flex flex-row mb-3 p-3 justify-content-around">
-            <div><a href="https://leone.tec.br/games/tic-tac-toe">
-                    <button class="btn btn-outline-dark bg-lg">Jogar agora</button>
-                </a></div>
-            <div><a href="https://github.com/leonetecbr/tic-tac-toe">
-                    <button class="btn btn-outline-dark bg-lg">Ver código</button>
-                </a></div>
+            <div>
+                <a href="https://leone.tec.br/games/tic-tac-toe" class="btn btn-outline-dark bg-lg">Jogar agora</a>
+            </div>
+            <div>
+                <a href="https://github.com/leonetecbr/tic-tac-toe" class="btn btn-outline-dark bg-lg">Ver código</a>
+            </div>
         </div>
     </div>
 </div>
+<!--div class="bg-dark mr-md-3 px-3 px-md-5 text-center text-light overflow-hidden" id="resgate">
+    <div class="bg-light shadow-sm mx-auto p-3 mb-3"
+         style="width: 80%; max-width: 230px; height: 230px; border-radius: 0 0 21px 21px;">
+        <img src="/games/rescue/imgs/icons/192.png" alt="Jogo de resgate">
+    </div>
+    <h2 class="display-5">Jogo de Resgate</h2>
+    <div class="my-3 p-3">
+        <p class="lead">Seguindo as orientações do professor da Digital Innovation One, construir esse jogo onde o
+            objetivo é regatar o amigo e eliminar a maior quantidade de inimigos possíveis. A dificuldade aumenta
+            sempre que o jogador consegue mais pontos até ele perder todas suas energias, além disso, é possível
+            jogar <i>offline</i>.
+        </p>
+    </div>
+    <div class="d-flex flex-row mb-3 p-3 justify-content-around">
+        <div>
+            <a href="https://leone.tec.br/games/rescue" class="btn btn-outline-light bg-lg">Jogar agora</a>
+        </div>
+        <div>
+            <a href="https://github.com/leonetecbr/rescue-dio" class="btn btn-outline-light bg-lg">Ver código</a>
+        </div>
+    </div>
+</div-->
 <div class="text-center p-md-4 bg-dark text-light mb-3 p-3" id="blog">
     <h2 class="fs-2">Meu <i>Blog</i></h2>
 </div>
@@ -209,28 +228,30 @@
         que eu conhecia bem pouco, acabei aprendendo para poder desenvolver meu próprio tema.</p>
     <p>Ultimamente dei uma pausa nas postagens porque estou um pouco sem tempo de escrever, mas pretendo retornar em
         breve</p>
-    <div class="text-center"><a href="/blog" target="_blank">
-            <button class="btn btn-outline-dark btn-lg">Conhecer o meu <i>blog</i></button>
-        </a></div>
+    <div class="text-center">
+        <a href="/blog" target="_blank" class="btn btn-outline-dark btn-lg">Conhecer o meu <i>blog</i></a>
+    </div>
 </div>
-<hr>
-<footer class="container py-5">
-    <div class="row">
-        <div class="col-6 col-lg">
-            <small class="d-block mb-3 text-muted">&copy; 2020-<?php echo date('Y'); ?></small>
-            <small class="d-block mb-3 text-muted">Todos os direitos reservados</small>
+<footer class="container-fluid bg-dark text-light fw-light">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-6 col-lg">
+                <small class="d-block mb-3">&copy; 2020-<?php echo date('Y'); ?></small>
+                <small class="d-block mb-3">Todos os direitos reservados</small>
+            </div>
+            <div class="col-6 col-lg" id="social">
+                <h5>Redes sociais</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://www.linkedin.com/in/leonetecbr/">Linkedin</a></li>
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://github.com/leonetecbr">GitHub</a></li>
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://leone.tec.br/email">E-mail</a></li>
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://twitter.com/leonetecbr">Twitter</a></li>
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://www.instagram.com/leone.tec.br/">Instagram</a></li>
+                    <li><a class="text-reset text-decoration-none opacity-75" href="https://www.facebook.com/leonetecbr">Facebook</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="col-6 col-lg" id="social">
-            <h5>Redes sociais</h5>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="https://www.linkedin.com/in/leonetecbr/">Linkedin</a></li>
-                <li><a class="text-muted" href="https://github.com/leonetecbr">GitHub</a></li>
-                <li><a class="text-muted" href="https://leone.tec.br/email">E-mail</a></li>
-                <li><a class="text-muted" href="https://twitter.com/leonetecbr">Twitter</a></li>
-                <li><a class="text-muted" href="https://www.instagram.com/leone.tec.br/">Instagram</a></li>
-                <li><a class="text-muted" href="https://www.facebook.com/leonetecbr">Facebook</a></li>
-            </ul>
-        </div>
+    </div>
 </footer>
 <script src="/js/jquery-slim.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
